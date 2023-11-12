2bb20cabb8ac165574619128adb8cd00a37a9067
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-12 22:02:20 +0800

    feat: add styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: '@it-incubator/eslint-config',
+  extends: ["@it-incubator/eslint-config", "plugin:storybook/recommended"],
   overrides: [
     {
-      files: ['**/*.stories.tsx'],
+      files: ["**/*.stories.tsx"],
       rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        'no-console': 'off',
+        "react-hooks/rules-of-hooks": "off",
+        "no-console": "off",
       },
     },
   ],
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
+import "./styles/index.scss";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+
 import { App } from "./App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
