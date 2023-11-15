777f1e46c9023f132144b91fb60b66761022977d
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-15 18:57:44 +0800

    created checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Checkbox } from "@/components/ui/checkbox/checkbox";
+import { CheckboxUI } from "@/components/ui/checkbox/checkbox";
 
 export function App() {
   return (
     <div>
-      <Checkbox />
+      <CheckboxUI label={"asaa"} />
+      <CheckboxUI disabled label={"asaa"} />
     </div>
   );
 }
--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,6 +1,7 @@
+import { CheckMarkIcon } from "@/components/ui/checkbox/checkMarkIcon";
+import { Typography } from "@/components/ui/typography";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckboxProps, CheckedState } from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
 
 import s from "./checkbox.module.scss";
 
@@ -8,21 +9,43 @@ type CheckboxType = CheckboxProps & {
   checked?: CheckedState;
   className?: string;
   defaultChecked?: CheckedState;
+  disabled?: boolean;
+  label?: string | undefined;
   onCheckedChange?(checked: CheckedState): void;
-  required?: boolean;
-  text?: string | undefined;
 };
 
 export const CheckboxUI = (props: CheckboxType) => {
-  const { checked = false, className, text, ...rest } = props;
+  const {
+    checked = false,
+    className,
+    disabled = false,
+    label,
+    ...rest
+  } = props;
 
   return (
-    <>
-      <Checkbox.Root className={`${s.checkboxRoot} ${s.cla}`} {...rest}>
-        <Checkbox.Indicator className={s.checkboxIndicator}>
-          <CheckIcon />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
-    </>
+    <div style={{ alignItems: "center", display: "flex", marginRight: "15px" }}>
+      <div className={s.wrapper}>
+        <div className={s.around}>
+          <Checkbox.Root
+            className={s.checkboxRoot}
+            disabled={disabled}
+            {...rest}
+          >
+            <Checkbox.Indicator className={s.checkboxIndicator}>
+              <CheckMarkIcon color={disabled ? "#dad9df" : "black"} />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+        </div>
+      </div>
+      <Typography color={"white"}>
+        <label
+          className={`${s.label} ${disabled ? s.labelDisabled : ""}`}
+          htmlFor={"c1"}
+        >
+          Accept terms and conditions.
+        </label>
+      </Typography>
+    </div>
   );
 };
