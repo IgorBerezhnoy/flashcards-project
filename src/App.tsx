42b9197c197eed44c3e08b6bcbfc4949dc042f98
Alexandr Naum <mfq07k9cz@gmail.com>
2023-11-16 12:45:29 +0300

    created Slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Checkbox } from '@/components/ui/checkbox/checkbox'
+import {Slider} from "@/components/ui/slider";
 
 export function App() {
   return (
     <div>
       <Checkbox label={'asaa'} />
       <Checkbox disabled label={'asaa'} />
+        <Slider value={[10, 50]}/>
     </div>
   )
 }
---
+++ src/components/ui/slider/slider.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Slider } from './slider'
+
+const meta = {
+  component: Slider,
+  tags: ['autodocs'],
+  title: 'Components/UI/Slider',
+} satisfies Meta<typeof Slider>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SliderTest: Story = {
+  args: {
+    value: [20, 59],
+  },
+}
