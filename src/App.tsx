1bc71b579a96c7e9831b0f3fc57916707fec29fc
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-17 21:46:21 +0800

    finished radioGroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import { RadioGroup } from '@/components/ui/radio'
-import { Slider } from '@/components/ui/slider'
-
 export function App() {
-  return (
-    <div>
-      Э
-      <RadioGroup />
-      <Slider value={[10, 50]} />
-    </div>
-  )
+  return <div></div>
 }
