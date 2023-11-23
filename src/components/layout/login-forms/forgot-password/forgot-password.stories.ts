f7645973613193e28f1d6a33b0a9191e585837c0
Alexandr Naum <mfq07k9cz@gmail.com>
2023-11-23 10:22:36 +0300

    created forgot password form

---
+++ src/components/layout/login-forms/forgot-password/forgot-password.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ForgotPassword } from './'
+
+const meta = {
+  component: ForgotPassword,
+  tags: ['autodocs'],
+  title: 'Components/Layout/ForgotPassword',
+} satisfies Meta<typeof ForgotPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ForgotPasswordTest: Story = {
+  args: {
+    onSubmit: data => console.log(data),
+  },
+}
