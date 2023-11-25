49c8424d194e61f2f80904a2d82f9ef123fb31bb
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-11-25 16:49:02 +0300

    fix: change structure dialog (Sat, Nov 25, 2023, 4:48:33 PM)

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,23 +1,43 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
+import { Close } from '@/assets'
+import { DialogClose } from '@/components/ui/modal/modalClose'
+import { DialogTitle } from '@/components/ui/modal/modalTitle'
+import { Typography } from '@/components/ui/typography'
 import * as Dialog from '@radix-ui/react-dialog'
 
 import s from './modal.module.scss'
 
 type Props = {
-  children: ReactNode
-  trigger: ReactNode
+  children?: ReactNode
+  showHeader?: boolean
+  title?: string
+  trigger?: ReactNode
 } & ComponentPropsWithoutRef<typeof Dialog.Root>
 
 export const Modal = (props: Props) => {
-  const { children, trigger, ...rest } = props
+  const { children, showHeader = true, title, trigger, ...rest } = props
 
   return (
     <Dialog.Root {...rest}>
-      <Dialog.Trigger className={s.dialogTrigger}>{trigger}</Dialog.Trigger>
+      <Dialog.Trigger asChild className={s.dialogTrigger}>
+        {trigger}
+      </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay />
-        <Dialog.Content className={s.dialogContent}>{children}</Dialog.Content>
+        <Dialog.Content className={s.dialogContent}>
+          {showHeader && (
+            <div className={s.header}>
+              <DialogTitle asChild>
+                <Typography className={s.title}>{title}</Typography>
+              </DialogTitle>
+              <DialogClose className={s.closeButton}>
+                <Close />
+              </DialogClose>
+            </div>
+          )}
+          <div className={s.contentBlock}>{children}</div>
+        </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   )
