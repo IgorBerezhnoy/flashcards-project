4668dde5fbc14f80f57ca9541c96d16e285908e6
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-29 22:23:56 +0800

    created Learn Card

--- src/services/flashcards.types.ts
+++ src/services/flashcards.types.ts
@@ -86,3 +86,19 @@ export interface DeckItem {
   updated: string
   userId: string
 }
+
+export interface LearnCardsResponse {
+  answer: string
+  answerImg: string
+  answerVideo: string
+  created: string
+  deckId: string
+  id: string
+  question: string
+  questionImg: string
+  questionVideo: string
+  rating: number
+  shots: number
+  updated: string
+  userId: string
+}
