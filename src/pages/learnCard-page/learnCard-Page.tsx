ec5b56d4b4f0c66ebea243d6284ba65d26291927
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-12-11 19:41:48 +0800

    corrected learn cards

--- src/pages/learnCard-page/learnCard-Page.tsx
+++ src/pages/learnCard-page/learnCard-Page.tsx
@@ -1,16 +1,61 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
 import { BackToPacksList } from '@/components/layout/backToPack'
 import { CurrentCard } from '@/components/layout/learnCard/currentCard'
+import { Loader } from '@/components/ui'
+import { useActions } from '@/hooks'
+import { currentCardActions } from '@/services/cards/card.slice'
+import { useLearnCardsQuery } from '@/services/decks/decks.service'
+import { ErrorType } from '@/services/decks/decks.types'
 
 import s from './learnCard-page.module.scss'
 
 export const LearnCardPage = () => {
+  const { id } = useParams()
+  const { setCurrentCard } = useActions(currentCardActions)
+
+  const { data, error, isError, isLoading } = useLearnCardsQuery(id ?? '')
+
+  useEffect(() => {
+    console.log(1)
+    if (data) {
+      setCurrentCard({
+        currentCard: {
+          answer: {
+            img: data?.answerImg,
+            text: data?.answer,
+          },
+          question: {
+            img: data?.questionImg,
+            text: data?.question,
+          },
+          shots: data?.shots,
+        },
+      })
+    }
+  }, [data])
+  if (isLoading) {
+    return <Loader />
+  }
+  if (isError) {
+    const err = error as ErrorType
+
+    toast.error(err?.data?.message)
+  }
+
   return (
     <div className={s.learnCard}>
       <div className={`${s.learnCard__box} learnCard__box`}>
         <div>
           <BackToPacksList />
         </div>
-        <CurrentCard />
+        {isError ? (
+          <h1 className={s.headerError}>There are no cards in the deck.</h1>
+        ) : (
+          <CurrentCard id={data?.id ?? ''} />
+        )}
       </div>
     </div>
   )
